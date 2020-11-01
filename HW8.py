a8242ee2b966b63fb907bfe22d0b7153230d7351
Lucas Sward <loss129@yahoo.com>
2020-11-01 15:51:19 -0700

    Deleted comments and imported numpy

--- HW8.py
+++ HW8.py
@@ -3,38 +3,26 @@ import fractions
 import random
 import time 
 import sys
+import numpy as np
+
 def findprimefactors(n):
 	timer = time.time()
-	#backup = 2
-	factor = 2
+	factor = True
 	# Start of the while loop to start the algorithm
-	print 'Running Without Backup'
-	while (factor):				
-		# This is my backup method to my attempt to implement Pollard's Rho algorithm
-		#if(n%backup == 0):
-			#print 'One factor: ', backup
-		#	print 'Other factor: ',n/backup
-		#	print 'For the number: ', n
-		#	print("%s Seconds Elapsed"%(time.time()-timer))
-		#	break
-		# Incrementing backup so that it gets all the odd numbers from [2,n)
-		#else: backup = backup+2
-		# Adjustment for adding 2 to 2
-		#if (backup == 4):
-		#	backup = 3
+	while (factor):
  #Get a random number within n for x
 		x = random.randint(2,n-1)
  #Get a random number within n for r
 		r = random.randint(2,n-1)		
 	# If the greatest common denomninator is greater than one then we have found a factor 
-		factor = fractions.gcd(abs(x-r), n)
+		factor = fractions.gcd(np.absolute(x-r), n)
 		if (factor > 1):
 		# We only need one factor in order to find the other so we print the factors and break the while loop
-			print 'One factor: ', factor
-			print 'Other factor: ',n/factor
-			print 'For the number: ', n		
+			print(f'One factor: {factor}')
+			print(f'Other factor: {n/factor}')
+			print(f'For the number: {n}')		
 			print("%s Seconds Elapsed"%(time.time()-timer))
 			break
 # line to take the number from the command line
-findprimefactors(int(sys.argv[1]))
+findprimefactors(int(60477244035776630857))
 	
